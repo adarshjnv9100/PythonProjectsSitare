16611396f569b1fc8f30e312fae6739b524dab10
adarshjnv9100 <adarshjnv9100@gmail.com>
2025-09-24 10:31:04 +0530

    Delete 19sep directory

--- 19sep/01.py
+++
@@ -1,7 +0,0 @@
-l = eval(input('Enter the list: '))
-nl = []
-for i in l:
-    if i not in nl:
-        nl.append(i)
-
-print(nl)
--- 19sep/04.py
+++
@@ -1,4 +0,0 @@
-with open('C://Users//Administrator//Desktop/Programming methodology in python//project//19sep//error.txt') as f:
-    for line in f:
-        if 'error' in line.lower():
-            print(line.strip())
